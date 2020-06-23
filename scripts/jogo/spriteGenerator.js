b1c97c767386510e1630380d7e5ed7196cbdcb8e
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2020-06-23 00:13:31 -0300

    improvements on the algorithm

--- scripts/jogo/spriteGenerator.js
+++ scripts/jogo/spriteGenerator.js
@@ -2,14 +2,17 @@ class SpriteGenerator{
     //Class used to generate the sprite array.
     //  we need the block horizontal and vertical positions, and the size of the spritesheet.
     constructor(spriteOffsetX,spriteOffsetY,spriteFrameCount,spriteFrameLine){
+
         let a = [];
-        for(let y=0; y<(spriteFrameCount/spriteFrameLine);y++){
-            for(let x=0; x<spriteFrameLine; x++){            
-                a.push([spriteOffsetX * (x==spriteFrameLine?0:x), 
-                        spriteOffsetY * (y==spriteFrameCount/spriteFrameLine?0:y) ]);
+
+        for(let y=0; y < Math.round(spriteFrameCount/spriteFrameLine); y++){
+            for(let x=0; x < spriteFrameLine; x++){            
+                a.push([spriteOffsetX * x, 
+                        spriteOffsetY * y]);
 
             }
         }
+
         this.SpriteArray = a;
         
     }
